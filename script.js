fb0cefb10001f30b42f350985d086dbec7c12bb6
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-03-15 12:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
+window.promises=[];
+window.promises = Array.from({ length: 5 }, () => new Promise(resolve => {
+  const timeout = Math.floor(Math.random() * 5) + 1;
+  setTimeout(() => resolve(`Promise resolved after ${timeout} seconds`), timeout * 1000);
+}));
 
-let promise1=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise1");
-	},(Math.floor(Math.random()*(5)) + 1)*1000;
-});
-let promise2=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise2");
-	},(Math.floor(Math.random()*(5)) + 1)*1000;
-});
-let promise3=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise3");
-	},(Math.floor(Math.random()*(5)) + 1)*1000;
-});
-let promise4=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise4");
-	},(Math.floor(Math.random()*(5)) + 1)*1000;
-});
-let promise5=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise5");
-	},(Math.floor(Math.random()*(5)) + 1)*1000;
-});
-window.promises = [promise1,promise2,promise3,promise4,promise5];
-
-let ans=Promise.Any(window.promises)
-	.then((res) => {
-    console.log("ANY", res);
-  }).catch((e) => console.log(e));
-
-let body=document.getElementById("output");
-body.innerHTML=ans;
+Promise.any(window.promises)
+  .then(result => document.getElementById("output").innerHTML = result);
 
 
 
